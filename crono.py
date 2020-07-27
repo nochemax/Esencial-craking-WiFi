f9c85aa03ddecbdbb88464962922461d45d0749a
David <sotodelanoche@gmail.com>
2020-07-27 20:12:43 +0000

    Update crono.py

--- crono.py
+++ crono.py
@@ -14,13 +14,13 @@ import os, time, threading, io
 os.system('clear')
 print("\033[1;31;1m ")
 os.system('figlet        Smp_A')
-print("			  	   Black_Hack")                 	
+print("		 Black_Hack")                     	
 print("\033[1;37;1m ")
 #&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
 
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ Variables principales $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-listamenu=["opciones:","1) selec Wlan & changer Mac" ,"2) Scaner red ", "3) CronoS WiFi","4) Exit"]#Menu Princcipal
+listamenu=["Opciones:","1) Selec Wlan & changer Mac" ,"2) Scaner red ", "3) CronoS WiFi","4) Exit"]#Menu Princcipal
 key=0
 exit=False
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ OPciones MEnu ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -42,19 +42,21 @@ def config():
 			os.system("ifconfig")
 			wlan=input("Introduzca Wlan: ")
 			print("Procesando")
-			os.system('ip link set '+wlan+' up')
 			os.system('airmon-ng check kill')
 			os.system('ifconfig '+wlan+' down')
+			os.system('iwconfig wlan0 mode monitor')
 			os.system('macchanger -A '+wlan)
 			os.system('ifconfig '+wlan+' up') 
 			os.system('airmon-ng start '+wlan)
+			if(wlan!='wlan0mon'): #falta condicion or
+				os.system('airmon-ng start '+wlan)
+			print(wlan)	
 			return wlan
 			break
 		except TypeError:
 			print("Wlan no permitida")
 
 def scan(wlan):
-
 	os.system('airodump-ng -i '+wlan+' --wps --manufacturer')
 
 def attack(wlan):
@@ -74,10 +76,10 @@ while exit==False:
 	elif (key==3):
 		attack(wlan)
 	elif (key==4):
-		#os.syste('airmon-ng stop wlan0mon')
-		#os.syste('ifconfig wlan0 down')
-		#os.syste('iwconfig wlan0 mode managed')
-		#os.syste('ifconfig wlan0 up')
-		#os.system('service network-manager start')
+		os.system('airmon-ng stop wlan0mon')
+		os.system('ifconfig wlan0 down')
+		os.system('iwconfig wlan0 mode managed')
+		os.system('ifconfig wlan0 up')
+		os.system('service network-manager start')
 		exit=True
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
